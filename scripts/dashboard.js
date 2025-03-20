1e56116d98b4fdd076e324a87e276d86d1139f7c
Henry Moran <135266041+plobethus@users.noreply.github.com>
2025-03-20 14:35:41 -0500

    removed load packages on dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,28 +1,22 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const token = sessionStorage.getItem("token");
-    const role = sessionStorage.getItem("role");
-  
-    if (!token || !role) {
-      window.location.href = "../login.html";
-      return;
-    }
-  
-    const welcomeDiv = document.getElementById("welcome-message");
-    if (welcomeDiv) {
-      const name = sessionStorage.getItem("name") || "User";
-      welcomeDiv.innerText = `Welcome, ${name} (${role === "customer" ? "Customer" : "Employee"})!`;
-    }
-  
-    if (role === "employee") {
-      try {
-        await loadPackages();
-      } catch (error) {
-        console.error("Error loading packages on page load:", error);
-      }
-    }
-  });
-  
-  function logout() {
-    sessionStorage.clear();
+document.addEventListener("DOMContentLoaded", () => {
+  const token = sessionStorage.getItem("token");
+  const role = sessionStorage.getItem("role");
+
+  if (!token || !role) {
     window.location.href = "../login.html";
+    return;
   }
+
+  const welcomeDiv = document.getElementById("welcome-message");
+  if (welcomeDiv) {
+    const name = sessionStorage.getItem("name") || "User";
+    welcomeDiv.innerText = `Welcome, ${name} (${role === "customer" ? "Customer" : "Employee"})!`;
+  }
+
+  
+});
+
+function logout() {
+  sessionStorage.clear();
+  window.location.href = "../login.html";
+}
